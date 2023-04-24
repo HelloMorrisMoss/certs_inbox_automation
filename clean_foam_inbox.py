17a3b688f4c6d1ceb9a9fb8ae03b7d1858d83d4f
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2023-04-24 15:33:11 -0400

    Move process script to new main_process.py

--- clean_foam_inbox.py
+++ clean_foam_inbox.py
@@ -7,11 +7,9 @@ from typing import List, Dict, Any
 import pandas as pd
 import win32com
 
-from helpers.json_help import df_json_handler
-from helpers.outlook_helpers import add_categories_to_mail, find_folders_in_outlook, remove_categories_from_mail
+from helpers.outlook_helpers import add_categories_to_mail, remove_categories_from_mail
 from log_setup import lg
 from outlook_interface import wc_outlook
-from untracked_config.accounts_and_folder_paths import acct_path_dct
 from untracked_config.subject_regex import subject_pattern
 
 now = datetime.datetime.now()
@@ -267,7 +265,7 @@ def compare_keep_and_move(mirs, kirs, unmatched):
     return unmatched
 
 
-def color_foam_groups(dfg, move_items, move_item_color):
+def color_foam_groups(dfg, move_items, move_item_color, valid_colors):
     colorize_series(move_items, move_item_color)
     for color, (group_name, group_df) in zip(valid_colors, dfg):
         print(f'{color=}, {len(group_df)}')
@@ -285,50 +283,3 @@ def clear_test_foam_group_colors(dfg, test_colors):
             o_item.Categories = ''
             o_item.Save()
 
-
-if __name__ == '__main__':
-    # ### this section is for development and demonstration only ###
-
-    # pandas display settings for development
-    pd.set_option('display.max_rows', 100)
-    pd.set_option('display.max_columns', 100)
-    pd.set_option('display.width', 1000)
-
-    import json
-    from helpers.outlook_helpers import valid_colors
-
-    # a summary debug info dictionary
-    smry = dict(checked_folders={}, skipped_folders=[], all_subj_lines=[], matched=[], missing_a_match=[])
-
-    account_name = acct_path_dct['account_name']
-    production_inbox_folders = acct_path_dct['inbox_folders']
-
-    found_folders_dict = find_folders_in_outlook(wc_outlook, account_name, production_inbox_folders)
-    pfdfs = get_process_folders_dfs(account_name, production_inbox_folders, found_folders_dict)
-    unmatched = []  # for checking for unmatched items
-    testing_colors_move = ['grey']
-    found_folders_keys = found_folders_dict.keys()
-
-    for df, folder_path in pfdfs:
-        if folder_path in found_folders_keys:
-            move_item_rows, keep_item_rows, dfg = group_foam_mail(df, folder_path, smry)
-            unmatched = compare_keep_and_move(move_item_rows, keep_item_rows, unmatched)
-            move_items = get_mail_items_from_results(move_item_rows)
-            color_foam_groups(dfg, move_items, move_item_color=testing_colors_move)
-            pass
-            clear_test_foam_group_colors(dfg, test_colors=valid_colors)
-        else:
-            lg.warn(f'Missing {folder_path} in checked folders!')
-
-    if unmatched:
-        lg.debug('UNMATCHED!!')
-        lg.debug(unmatched)
-    pass  # for breakpoint
-
-    # write the smry dictionary to a file to make it easier to look at
-    with open('./last_smry.json', 'w') as jf:
-        json.dump(smry, jf, indent=4, default=df_json_handler)
-
-# TODO: complete unit tests; next: a test confirming that the inbox looks like it does after "# color the groups"
-
-pass  # for breakpoint
